fe6268ef31d134c34c698e5e123fa00a4271eb23
Hicham1970 <devhicham2@gmail.com>
2025-02-10 21:30:51 +0100

    Impression en pdf réussie

--- src/functions/PrintToPdf.jsx
+++ src/functions/PrintToPdf.jsx
@@ -12,21 +12,28 @@ export default function PrintToPdf() {
             return;
         }
         setIsLoader(true);
-
-        html2canvas(capturePage)
+    
+        html2canvas(capturePage, {
+            backgroundColor: null,
+            scale: 2, // Increase the scale for better resolution
+        })
             .then((canvas) => {
                 const imgData = canvas.toDataURL("image/png");
                 const pdf = new jsPDF("p", "mm", "a4");
-
+    
                 const pageHeight = pdf.internal.pageSize.getHeight();
                 const pageWidth = pdf.internal.pageSize.getWidth();
-
+    
                 const imgWidth = pageWidth - 2 * 10;
                 const imgHeight = (imgWidth * canvas.height) / canvas.width;
                 const imgX = (pageWidth - imgWidth) / 2;
                 const imgY = (pageHeight - imgHeight) / 2;
                 pdf.addImage(imgData, "PNG", imgX, imgY, imgWidth, imgHeight);
-
+    
+                // Add borders around the content
+                pdf.setDrawColor(0); // Set border color to black
+                pdf.rect(imgX - 5, imgY - 5, imgWidth + 10, imgHeight + 10); // Draw border
+    
                 pdf.save("page.pdf");
                 setIsLoader(false);
             })
@@ -34,8 +41,7 @@ export default function PrintToPdf() {
                 console.error("Error capturing page:", error);
                 setIsLoader(false);
             });
-    };
-
+    };  
     // Call handleDownload when the component is used
     return handleDownload;
 }
